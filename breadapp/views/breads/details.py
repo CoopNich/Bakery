92b0a421cb898324fb571293ce4a857f9f761064
CoopNich <CoopNich92@gmail.com>
2020-05-17 10:17:20 -0500

    ingredients can be deleted from bread details

--- breadapp/views/breads/details.py
+++ breadapp/views/breads/details.py
@@ -16,3 +16,19 @@ def bread_details(request, bread_id):
         }
 
         return render(request, template, context)
+    if request.method == 'POST':
+        form_data = request.POST
+
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM breadapp_breadingredient
+                WHERE id = ?
+                """, (bread_id,))
+
+            return redirect(reverse('breadapp:bread_list'))
